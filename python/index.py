428228de2c78e287ad659f754acd2391c4370f27
mustafaAwwal <mustafaawwal1@gmail.com>
2025-06-12 21:22:19 +0500

    feat: python questions

---
+++ python/index.py
@@ -0,0 +1,17 @@
+# 1. Create a union type
+
+# 2. Create a type with annotation
+
+# 3. Create a typed dict
+
+# 4. How to create an optional field without using the Optional type helper
+
+# 5. How to pass a python dict as named arguments of a function
+
+# 6. How to pass an array as positional arguments to a python function
+
+# 7. How to filter through a list of items
+
+# 8. How to write inline if else conditions
+
+# 9. How to create an inline function in python
