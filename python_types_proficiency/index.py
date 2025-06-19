019f2b632fa42c340be782032a24b2081262be32
mustafaAwwal <mustafaawwal1@gmail.com>
2025-06-19 19:13:07 +0500

    fix: updated questions

---
+++ python_types_proficiency/index.py
@@ -0,0 +1,45 @@
+# 1. Create a union type
+a = int
+b = float 
+c = # Create a union of a and b types
+
+# 2. Create a type with annotation
+# Annotations are basically assigning a type and also providing additional information just for help. It does not change the actual types
+annotated_type = 
+
+# 3. Create a typed dict
+# Typed dict is a dict where the keys are of a certain type and the values are of a certain type
+typed_dict = # implement a typed dict here with three keys "number_value", "string_value", "boolean_value" and corresponding types as well
+
+
+# 4. Give two ways on how an optional type can be defined
+
+
+# 5. How to pass a python dict as named arguments of a function.
+# how to pass this parameters dict to the function without extracting individual values
+parameters = {
+    "a": 1,
+    "b": "hello"
+}
+def dict_parameters_function(a:int, b: str):
+    pass
+
+
+
+
+# 6. How to pass an array as positional arguments to a python function
+# How to pass this list_parameters to the function without extracting individual values
+list_parameters = ["a","b"]
+def list_parameters_function(a:int, b: str):
+    pass
+
+# 7. How to filter through a list of items in one line
+even_and_odd = [1,2,3,4,5,6,7,8,9,10]
+only_even = # implement an inline function to give only even numbers
+
+
+# 8. How to write inline if else conditions
+inline_if_else = # implement an inline if else condition here
+
+# 9. How to create an inline function in python
+inline_function = # implement an inline function here
