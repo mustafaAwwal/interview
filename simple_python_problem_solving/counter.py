bd4a9c889a50afd5f5e85afbc2196c6faac5db35
mustafaAwwal <mustafaawwal1@gmail.com>
2025-06-19 19:18:26 +0500

    fix: add simple problem solving

---
+++ simple_python_problem_solving/counter.py
@@ -0,0 +1,10 @@
+parameters = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]
+
+# Count the number of instances of each number in the parameters
+
+
+def count_the_number_of_instance(parameters):
+    pass
+
+
+print(count_the_number_of_instance(parameters))
---
+++ simple_python_problem_solving/remove_duplicates.py
@@ -0,0 +1,9 @@
+parameters = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]
+
+
+# remove duplicates from the given array
+def remove_duplicates(parameters: list[int]):
+    pass
+
+
+remove_duplicates(parameters)
